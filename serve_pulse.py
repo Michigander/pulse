af7098f15e3d18c8cb1c3c6d0173c839103f3877
Gordon Finnie <gordon.m.finnie.iii@gmail.com>
2020-06-26 23:48:54 -0400

    Rework for multiple connections

--- serve_pulse.py
+++ serve_pulse.py
@@ -1,45 +1,5 @@
 import asyncio
-import websockets
-import json
-
-
-class WebsocketServer:
-
-    def __init__(self):
-        self.host = 'localhost'
-        self.port = 5432
-        self.websocket = None
-
-    def serve(self):
-        return websockets.serve(self.serve_loop, self.host, self.port)
-
-    async def serve_loop(self, websocket, path):
-        self.websocket = websocket
-        async for message in websocket:
-            await self.on_receive(message)
-
-    def on_receive(self, message):
-        # implement me
-        pass
-
-
-class PulseServer(WebsocketServer):
-
-    def __init__(self):
-        super().__init__()
-        self.clients = []
-
-    async def on_receive(self, raw_message):
-        message = json.loads(raw_message)
-        print(f'[log] received: {message}')
-
-        if message['type'] == 'connect':
-            await self.on_connect(message)
-
-    async def on_connect(self, message):
-        client_id = len(self.clients) + 1
-        self.clients.append(client_id)
-        await self.websocket.send(f'YEETster #{client_id}')
+from pulse_server import PulseServer
 
 
 if __name__ == "__main__":
